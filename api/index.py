a91ec261f218f27fa57206de34c4682ca2aa74ad
fernandocaselli <40582989+fernandocaselli@users.noreply.github.com>
2023-05-27 23:46:58 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    resp = make_response("Hello, World!")
+    resp = make_response("Hello, World 2!")
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
 @app.route('/about')
 def about():
-    resp = make_response("About")
+    resp = make_response("About 2")
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
